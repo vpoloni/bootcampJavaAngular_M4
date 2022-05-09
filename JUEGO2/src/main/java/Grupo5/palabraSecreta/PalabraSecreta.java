08a4a16b9c1720e492a411b9f0a51f82635977ae
MarcNebot1git commit -m https://github.com/MarcNebot1/MarcNebot1.git <newgate3211@gmail.com>
2022-05-09 02:54:05 +0200

    Acabado imagenes ahorcado

--- JUEGO2/src/main/java/Grupo5/palabraSecreta/PalabraSecreta.java
+++ JUEGO2/src/main/java/Grupo5/palabraSecreta/PalabraSecreta.java
@@ -1,10 +1,12 @@
 package Grupo5.palabraSecreta;
 
 import javax.swing.JFrame;
+
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -17,10 +19,19 @@ public class PalabraSecreta extends JFrame {
 
 	private JPanel contentPane;
 
+	private int contadorimagenes = 9;
 	private int contador = 11;
 	private int contador_2 = 0;
+	private int fallos = 0;
+
 	private JButton btnNext;
 	private JLabel LabelPSecreta = new JLabel("");
+
+	private JLabel NewLabel0, NewLabel1, NewLabel2, NewLabel3, NewLabel4, NewLabel5, NewLabel6, NewLabel7, NewLabel8,
+			NewLabel9, NewLabel10;
+	ArrayList<JLabel> arrayName = new ArrayList<JLabel>();
+
+	// arrayName = new ArrayList<JLabel>[9];
 
 	// arrayList para agrupar los botones
 	private ArrayList<JButton> botonesTeclado = new ArrayList<JButton>();
@@ -35,11 +46,12 @@ public class PalabraSecreta extends JFrame {
 
 	private JLabel LabelContador;
 	private JLabel lblNewLabel_6;
+	JLabel labelimage = new JLabel("");
 
 	public PalabraSecreta() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 550, 600);
+		setBounds(100, 100, 750, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -49,6 +61,17 @@ public class PalabraSecreta extends JFrame {
 		JButton Button_inicio = new JButton("Iniciar juego");
 		Button_inicio.setBounds(50, 50, 200, 50);
 		contentPane.add(Button_inicio);
+		// al pulsar el botón de inicio, habilitamos los botones
+		Button_inicio.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// true para habilitar los botones
+				reiniciarimagenes();
+				habilitarODeshabilitar(true);
+				inicializarArray();
+			}
+
+		});
 
 		JButton Button_Resolver = new JButton("Resolver");
 		Button_Resolver.setBounds(50, 100, 200, 50);
@@ -75,6 +98,8 @@ public class PalabraSecreta extends JFrame {
 		int x = 10;
 		int y = 347;
 		int contadorSaltos = 0;
+
+		arrayName = new ArrayList<JLabel>(contadorimagenes);
 
 		String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
@@ -107,11 +132,68 @@ public class PalabraSecreta extends JFrame {
 		JLabel lblNewLabel_4 = new JLabel("Palabra secreta");
 		lblNewLabel_4.setBounds(10, 264, 96, 14);
 		contentPane.add(lblNewLabel_4);
+		
+		arrayName.clear();
+
+		NewLabel0 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/0.png")));
+		NewLabel1 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/1.png")));
+		NewLabel2 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/2.png")));
+		NewLabel3 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/3.png")));
+		NewLabel4 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/4.png")));
+		NewLabel5 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/5.png")));
+		NewLabel6 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/6.png")));
+		NewLabel7 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/7.png")));
+		NewLabel8 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/8.png")));
+		NewLabel9 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/9.png")));
+		NewLabel10 = new JLabel(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/10.png")));
+
+		NewLabel0.setBounds(304, 11, 362, 546);
+		NewLabel1.setBounds(304, 11, 362, 546);
+		NewLabel2.setBounds(304, 11, 362, 546);
+		NewLabel3.setBounds(304, 11, 362, 546);
+		NewLabel4.setBounds(304, 11, 362, 546);
+		NewLabel5.setBounds(304, 11, 362, 546);
+		NewLabel6.setBounds(304, 11, 362, 546);
+		NewLabel7.setBounds(304, 11, 362, 546);
+		NewLabel8.setBounds(304, 11, 362, 546);
+		NewLabel9.setBounds(304, 11, 362, 546);
+		NewLabel10.setBounds(304, 11, 362, 546);
+
+		contentPane.add(NewLabel0);
+		contentPane.add(NewLabel1);
+		contentPane.add(NewLabel2);
+		contentPane.add(NewLabel3);
+		contentPane.add(NewLabel4);
+		contentPane.add(NewLabel5);
+		contentPane.add(NewLabel6);
+		contentPane.add(NewLabel7);
+		contentPane.add(NewLabel8);
+		contentPane.add(NewLabel9);
+		contentPane.add(NewLabel10);
+
+		arrayName.add(0, NewLabel0);
+		arrayName.add(1, NewLabel1);
+		arrayName.add(2, NewLabel2);
+		arrayName.add(3, NewLabel3);
+		arrayName.add(4, NewLabel4);
+		arrayName.add(5, NewLabel5);
+		arrayName.add(6, NewLabel6);
+		arrayName.add(7, NewLabel7);
+		arrayName.add(8, NewLabel8);
+		arrayName.add(9, NewLabel9);
+		arrayName.add(10, NewLabel10);
+
+		
+
+		labelimage.setBounds(10, 264, 96, 14);
+		contentPane.add(labelimage);
+		labelimage.setVisible(false);
 
 		final JButton btnNewButton_2 = new JButton("");
 		btnNewButton_2.setBounds(10, 198, 36, 35);
 		btnNewButton_2.setIcon(new ImageIcon(PalabraSecreta.class.getResource("/Grupo5/img/Untitled.png")));
 		contentPane.add(btnNewButton_2);
+
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btnNewButton_2.setVisible(false);
@@ -178,8 +260,6 @@ public class PalabraSecreta extends JFrame {
 			}
 		});
 
-		
-		
 		LabelContador = new JLabel("");
 		LabelContador.setBounds(191, 264, 22, 14);
 		contentPane.add(LabelContador);
@@ -192,6 +272,7 @@ public class PalabraSecreta extends JFrame {
 		btnNext.setVisible(false);
 		btnNext.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				reiniciarimagenes();
 				btnNext.setVisible(false);
 				habilitarODeshabilitar(true);
 				agregarListenersBotones();
@@ -199,23 +280,14 @@ public class PalabraSecreta extends JFrame {
 				añadir(palabrasSecretas());
 				inicializarArray();
 				contador();
-	
+				
+					
+				
 
 			}
 		});
 		btnNext.setBounds(134, 515, 149, 23);
 		contentPane.add(btnNext);
-
-		// al pulsar el botón de inicio, habilitamos los botones
-		Button_inicio.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// true para habilitar los botones
-				habilitarODeshabilitar(true);
-				inicializarArray();
-			}
-
-		});
 
 	}
 
@@ -240,11 +312,12 @@ public class PalabraSecreta extends JFrame {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					botonesTeclado.get(indxBtn).setEnabled(false);
-
+					
 					insertarLetras(botonesTeclado.get(indxBtn));
-
+					insertarImagen(botonesTeclado.get(indxBtn));
 				}
 			});
+		
 		}
 	}
 
@@ -253,12 +326,12 @@ public class PalabraSecreta extends JFrame {
 
 		for (int n = 0; n < letras_palabra.size(); n++) {
 
-			if (a.getText().equals(letras_palabra.get(n).toString())) {
+			if (a.getText().toLowerCase().equals(letras_palabra.get(n).toString())) {
 
-				igual = true;
+				return  true;
 			}
 		}
-		return igual;
+		return false;
 	}
 
 	public String palabrasSecretas() {
@@ -275,21 +348,21 @@ public class PalabraSecreta extends JFrame {
 		palabras.add("ecualizador");
 		palabras.add("espectador");
 
-		if(contador==11) {
-			
-		    int pos;
-		    int nNumeros = 10;
-		    
-		    for (int i = 0; i < nNumeros ; i++) {
-		      pos = (int) Math.floor(Math.random() * nNumeros );
-		      while (sinRepetir.contains(pos)) {
-		        pos = (int) Math.floor(Math.random() * nNumeros );
-		      }
-		      sinRepetir.push(pos);
-		    }
-		    System.out.println(sinRepetir.toString());
+		if (contador == 11) {
+
+			int pos;
+			int nNumeros = 10;
+
+			for (int i = 0; i < nNumeros; i++) {
+				pos = (int) Math.floor(Math.random() * nNumeros);
+				while (sinRepetir.contains(pos)) {
+					pos = (int) Math.floor(Math.random() * nNumeros);
+				}
+				sinRepetir.push(pos);
+			}
+			System.out.println(sinRepetir.toString());
 		}
-		
+
 		return palabras.get(sinRepetir.get(contador_2));
 
 	}
@@ -334,16 +407,50 @@ public class PalabraSecreta extends JFrame {
 		for (int i = 0; i < letras_palabra.size(); i++) {
 
 			if (letras_palabra.get(i) == indexBtn.getName().charAt(0)) {
-				//System.out.println("QUE ES GETNAME " + indexBtn.getName());
+				// System.out.println("QUE ES GETNAME " + indexBtn.getName());
 				letras_panel.set(i, indexBtn.getName().charAt(0));
 			}
-			//System.out.println("LETRAS PANEL" + letras_panel);
+			// System.out.println("LETRAS PANEL" + letras_panel);
 		}
 
 		LabelPSecreta.setText(String.valueOf(letras_panel));
 
 		if (!letras_panel.contains('_')) {
 			btnNext.setVisible(true);
+		}
+	}
+
+	public void insertarImagen(JButton indexBtn) {
+
+//TODO:resetear var fallo cuando llege a 10 adivine la palabra
+		if (!verificar(indexBtn)) {
+			fallos++;
+			labelimage = arrayName.get(fallos);
+			labelimage.setBounds(300, 25, 250, 320);
+			
+			contentPane.add(labelimage);
+			if(fallos != 0) {
+				arrayName.get(fallos-1).setVisible(false);	
+			}
+			arrayName.get(fallos).setVisible(true);	
+
+		}
+		if (fallos == arrayName.size()-1) {
+			JOptionPane.showMessageDialog(null, "HAS PERDIDO! XD");
+			btnNext.setVisible(true);
+			fallos=0;
+			
+		}
+	
+
+	}
+
+	public void reiniciarimagenes() {
+		for (int i = 0; i < arrayName.size(); i++) {
+			if (i == 0) {
+				arrayName.get(i).setVisible(true);
+			}
+			arrayName.get(i).setVisible(false);
 		}
 	}
 }
